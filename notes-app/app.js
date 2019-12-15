1a2f324b59127c0a13ec66ae61139fa4c7191c32
Kevin Labtani <kevinlabtani@gmail.com>
2019-12-15 20:26:55 +0100

    using yargs and setting up commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,52 @@
 const chalk = require("chalk");
+const yargs = require("yargs");
 const getNotes = require("./notes.js");
 
-const command = process.argv[2]; // node app.js add => we get 'add'
+// node app.js --help provided by yargs
 
-if (command === "add") {
-  console.log("Adding note!");
-} else if (command === "remove") {
-  console.log("removing note!");
-}
+// customize yargs version
+yargs.version("1.1.0");
+
+// create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  // handler called when we use the function
+  handler: function() {
+    console.log("Adding a new note!");
+  },
+});
+
+// create read command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  // handler called when we use the function
+  handler: function() {
+    console.log("Reading a new note!");
+  },
+});
+
+// create list command
+yargs.command({
+  command: "list",
+  describe: "Listing all your notes",
+  // handler called when we use the function
+  handler: function() {
+    console.log("Listing out your notes!");
+  },
+});
+
+// create read command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  // handler called when we use the function
+  handler: function() {
+    console.log("Removing a new note!");
+  },
+});
+
+// node app.js add --title="things to buy"
+console.log(yargs.argv);
+// { _: [ 'add' ], title: 'things to buy', '$0': 'app.js' }
