4b82cc34109302dbc141d373fb8aa07ffefb6149
Kevin Labtani <kevinlabtani@gmail.com>
2019-12-15 20:46:04 +0100

    yargs builder

--- notes-app/app.js
+++ notes-app/app.js
@@ -8,12 +8,26 @@ const getNotes = require("./notes.js");
 yargs.version("1.1.0");
 
 // create add command
+// call with node app.js add --title="Shopping list" --body="buy soap"
+// builder set up option for command
 yargs.command({
   command: "add",
   describe: "Add a new note",
-  // handler called when we use the function
-  handler: function() {
-    console.log("Adding a new note!");
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function(argv) {
+    console.log(`Title: ${argv.title}`);
+    console.log(`Body: ${argv.body}`);
   },
 });
 
@@ -21,7 +35,6 @@ yargs.command({
 yargs.command({
   command: "read",
   describe: "Read a note",
-  // handler called when we use the function
   handler: function() {
     console.log("Reading a new note!");
   },
@@ -31,7 +44,6 @@ yargs.command({
 yargs.command({
   command: "list",
   describe: "Listing all your notes",
-  // handler called when we use the function
   handler: function() {
     console.log("Listing out your notes!");
   },
@@ -41,12 +53,11 @@ yargs.command({
 yargs.command({
   command: "remove",
   describe: "Remove a note",
-  // handler called when we use the function
   handler: function() {
     console.log("Removing a new note!");
   },
 });
 
-// node app.js add --title="things to buy"
-console.log(yargs.argv);
-// { _: [ 'add' ], title: 'things to buy', '$0': 'app.js' }
+// need to tell yeargs to do it's thing
+// console.log(yargs.argv); used to do it
+yargs.parse();
