66c8f2467442aff9b62ca7d065245a7508fe17f6
Kevin Labtani <kevinlabtani@gmail.com>
2019-12-15 19:54:48 +0100

    install nodemon gloabally

--- notes-app/app.js
+++ notes-app/app.js
@@ -5,4 +5,7 @@ const msg = getNotes();
 
 console.log(msg);
 
-console.log(chalk.yellow.bgRed.bold("Success!"));
+console.log(chalk.black.bgRed.bold("Success!"));
+// npm install -g nodemon
+// nodemon app.js
+// Just use nodemon instead of node to run your code, and now your process will automatically restart when your code changes.
