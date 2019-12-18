d478c9db5d47add912ca7b39007e101545344047
Kevin Labtani <kevinlabtani@gmail.com>
2019-12-18 12:01:24 +0100

    add geocoding to get lat long

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,6 @@
 const request = require("request");
 
+// weather with darksky api
 const url =
   "https://api.darksky.net/forecast/a7107f851e36bca851ddb3e8a96c7799/50.4108,4.4446?units=si&lang=en";
 
@@ -11,4 +12,15 @@ request({ url: url, json: true }, (error, response, body) => {
     } degrees out. There is a ${body.currently.precipProbability *
       100}% chance of rain.`,
   );
+});
+
+// geocoding with mapbox api
+const geocodeURL =
+  "http://api.mapbox.com/geocoding/v5/mapbox.places/charleroi.json?access_token=pk.eyJ1Ijoia2V2aW5sYWJ0YW5pIiwiYSI6ImNrNGI0a2JsajA0eDAzanBpcm5uM3hrbzEifQ.VeXmb1HhNctRoyPkR8_odA&limit=1";
+
+// make the request and parse it as json
+request({ url: geocodeURL, json: true }, (error, response, body) => {
+  const latitude = body.features[0].center[1];
+  const longitude = body.features[0].center[0];
+  console.log(`long: ${longitude}, lat: ${latitude}`);
 });
