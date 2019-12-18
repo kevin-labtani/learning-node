e35590cccc2eedb35aa31bfe732ba504ce6c49d7
Kevin Labtani <kevinlabtani@gmail.com>
2019-12-18 10:47:25 +0100

    weather app: custom requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,14 @@
 const request = require("request");
 
 const url =
-  "https://api.darksky.net/forecast/a7107f851e36bca851ddb3e8a96c7799/37.8267,-122.4233";
+  "https://api.darksky.net/forecast/a7107f851e36bca851ddb3e8a96c7799/50.4108,4.4446?units=si&lang=en";
 
-request({ url }, (error, response, body) => {
-  const data = JSON.parse(body);
-  console.log(data);
+// make the request and parse it as json
+request({ url: url, json: true }, (error, response, body) => {
+  console.log(
+    `${body.daily.data[0].summary} It is currently ${
+      body.currently.temperature
+    } degrees out. There is a ${body.currently.precipProbability *
+      100}% chance of rain.`,
+  );
 });
