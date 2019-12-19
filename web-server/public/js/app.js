fb9fbb5ff8125a40fd1361d76c58907c33cdbdf0
Kevin Labtani <kevinlabtani@gmail.com>
2019-12-19 15:12:07 +0100

    web server: browser http request with fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,10 @@
-console.log("client side javascript file is loaded");
+fetch("http://localhost:3000/weather?address=charleroi")
+  .then(resp => resp.json())
+  .then(data => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data.location);
+      console.log(data.forecast);
+    }
+  });
