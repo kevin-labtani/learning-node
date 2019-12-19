e2fbb05b79d740d27a4b04611af3a17a7a0f9769
Kevin Labtani <kevinlabtani@gmail.com>
2019-12-19 12:53:39 +0100

    express: handlebars templating engine

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -8,8 +8,31 @@ const port = 3000;
 
 // setup to serve static file
 const publicDirectoryPath = path.join(__dirname, "../public");
-
 app.use(express.static(publicDirectoryPath));
+
+// set handlebars as view engine
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  // render our handlebars template
+  res.render("index", {
+    title: "Weather",
+    name: "Kevin Labtani",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Kevin Labtani",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is some helpful text."
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({
