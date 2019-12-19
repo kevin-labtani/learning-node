2d34901ed62e57815d2c3dc25e9efeffcc77aa9b
Kevin Labtani <kevinlabtani@gmail.com>
2019-12-19 16:05:14 +0100

    web server: wiring up the UI

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,23 +1,28 @@
-// use fetch to gt wezther
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+
+const messageOne = document.querySelector("#message-one");
+const messageTwo = document.querySelector("#message-two");
+
+// use fetch to get weather
 const getWeather = location => {
   return fetch(`http://localhost:3000/weather?address=${location}`)
     .then(resp => resp.json())
     .then(data => {
       if (data.error) {
-        console.log(data.error);
+        messageOne.textContent = data.error;
       } else {
-        console.log(data.location);
-        console.log(data.forecast);
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
       }
     });
 };
 
 // set up form to get location from user input
-const weatherForm = document.querySelector("form");
-const search = document.querySelector("input");
-
 weatherForm.addEventListener("submit", e => {
   e.preventDefault();
   const location = search.value;
+  messageOne.textContent = "loading...";
+  messageTwo.textContent = "";
   getWeather(location);
 });
