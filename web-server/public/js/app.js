490647a1c7f2900aa5d1dffd771f4dfad609f136
Kevin Labtani <kevinlabtani@gmail.com>
2019-12-19 15:54:57 +0100

    web server: use search form to get user input

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,10 +1,23 @@
-fetch("http://localhost:3000/weather?address=charleroi")
-  .then(resp => resp.json())
-  .then(data => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
-    }
-  });
+// use fetch to gt wezther
+const getWeather = location => {
+  return fetch(`http://localhost:3000/weather?address=${location}`)
+    .then(resp => resp.json())
+    .then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        console.log(data.location);
+        console.log(data.forecast);
+      }
+    });
+};
+
+// set up form to get location from user input
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+
+weatherForm.addEventListener("submit", e => {
+  e.preventDefault();
+  const location = search.value;
+  getWeather(location);
+});
