209097a1bf78aa0edfd778e4d6e55dadb0885be0
Kevin Labtani <kevinlabtani@gmail.com>
2019-12-21 17:07:04 +0100

    task-manager: REST creation endpoint for users

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
 
 // connct to db with mongoose, create it if it doesn't exist
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
@@ -7,63 +6,6 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
-
-// create a model for our users
-const User = mongoose.model("User", {
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age must be a positive number");
-      }
-    },
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is invalid");
-      }
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 7,
-    validate(value) {
-      if (value.toLowerCase.includes("password")) {
-        throw new Error('Password cannot contain "password"');
-      }
-    },
-  },
-});
-
-// // create a user
-// const me = new User({
-//   name: "Kevin    ",
-//   email: "MYEMAIL@Hotmmai.com   ",
-//   password: "passWORD",
-// });
-
-// // save into the db, returns a promise
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//     //  __v stores the version of the document
-//   })
-//   .catch(error => {
-//     console.log("Error!", error);
-//   });
 
 const Task = mongoose.model("Task", {
   description: {
@@ -76,16 +18,3 @@ const Task = mongoose.model("Task", {
     default: false,
   },
 });
-
-const task = new Task({
-  description: "Feed cat    ",
-});
-
-task
-  .save()
-  .then(() => {
-    console.log(task);
-  })
-  .catch(error => {
-    console.log("Error: ", error);
-  });
