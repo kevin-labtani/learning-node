e8c1bf67fed997a85ba62900940e6e8a5cdd4ea1
Kevin Labtani <kevinlabtani@gmail.com>
2019-12-22 11:03:42 +0100

    task-manager: integrate async-await

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -13,89 +13,78 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // endpoint to create new user
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const user = new User(req.body);
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch(e => {
-      res.status(400).send(e);
-    });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
 // endpoint for reading users
-app.get("/users", (req, res) => {
-  User.find({})
-    .then(users => {
-      res.send(users); //array
-    })
-    .catch(e => {
-      res.status(500).send();
-    });
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users); //array
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
 // endpoint for reading a single user
 // express route parameter used to get dynamic part of the url
-app.get("/users/:id", (req, res) => {
+app.get("/users/:id", async (req, res) => {
   // mongoose auto convert string id into object ids
   const _id = req.params.id;
 
-  User.findById(_id)
-    .then(user => {
-      if (!user) {
-        // not getting anything back isn't an error for mongo so we have to handle the case
-        return res.status(404).send();
-      }
-
-      res.send(user);
-    })
-    .catch(e => {
-      return res.status(500).send();
-    });
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      // not getting anything back isn't an error for mongo so we have to handle the case
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    return res.status(500).send();
+  }
 });
 
 // endpoint to create a new task
-app.post("/tasks", (req, res) => {
+app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task);
-    })
-    .catch(e => {
-      res.status(400).send(e);
-    });
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
 // endpoint for reading tasks
-app.get("/tasks", (req, res) => {
-  Task.find({})
-    .then(tasks => {
-      res.send(tasks); //array
-    })
-    .catch(e => {
-      res.status(500).send();
-    });
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
 // endpoint for reading a single task
-app.get("/tasks/:id", (req, res) => {
+app.get("/tasks/:id", async (req, res) => {
   // mongoose auto convert string id into object ids
   const _id = req.params.id;
-
-  Task.findById(_id)
-    .then(task => {
-      if (!task) {
-        return res.status(404).send();
-      }
-
-      res.send(task);
-    })
-    .catch(e => {
-      return res.status(500).send();
-    });
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    return res.status(500).send();
+  }
 });
 
 app.listen(port, () => {
