231b20d51475e96860acccf60fddc77c55339acd
Kevin Labtani <kevinlabtani@gmail.com>
2019-12-22 20:10:18 +0100

    advanced postman

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,21 +8,8 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// start db with:
+// on dev, start db with:
 // mongod --dbpath=/home/user/Desktop/personal-projects/mead-node/task-manager/mongodb-data
-
-// express middleware
-// used to limit access
-// app.use((req, res, next) => {
-//   if (req.method === "GET") {
-//     res.send("GET requests are disabled");
-//   } else {
-//     next();
-//   }
-// });
-// app.use((req, res, next) => {
-//   res.status(503).send("The site is under maintenance");
-// });
 
 // parse incoming json to an object
 app.use(express.json());
--- task-manager/src/middleware/auth.js
+++ task-manager/src/middleware/auth.js
@@ -1,9 +1,16 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
+// to test in Postman, setup a header key:Authorization value: Bearer verylongtokenstring
+// or use authentification tab and set up Bearer token
+// or inherit from parent and set up Bearer token for the collection with {{authToken}} as token and then:
+// Postman code to put in test on create user and login route:
+// if (pm.response.code === 200) {
+//   pm.environment.set("authToken", pm.response.json().token);
+// }
+
 const auth = async (req, res, next) => {
   try {
-    // to test in Postman, setup a header key:Authorization value: Bearer verylongtokenstring
     const token = req.header("Authorization").replace("Bearer ", "");
     const decoded = jwt.verify(token, "thisismysecret00");
     // find the user with the correct id and who has that auth token still stored
