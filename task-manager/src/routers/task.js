246014846156a9e9dc1afa4dbadcc9b4cf89d69e
Kevin Labtani <kevinlabtani@gmail.com>
2019-12-23 14:02:32 +0100

    filtering tasks based on completed status

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -20,12 +20,23 @@ router.post("/tasks", auth, async (req, res) => {
 });
 
 // endpoint for reading tasks
+// GET /tasks?completed=true
 router.get("/tasks", auth, async (req, res) => {
+  // 3 options: no query params, or completed=true or completed=false
+  const match = {};
+
+  if (req.query.completed) {
+    // convert from string to bool
+    match.completed = req.query.completed === "true";
+  }
+
   try {
-    // const tasks = await Task.find({ owner: req.user._id });
-    // res.send(tasks);
-    // alternative:
-    await req.user.populate("tasks").execPopulate();
+    await req.user
+      .populate({
+        path: "tasks",
+        match,
+      })
+      .execPopulate();
     res.send(req.user.tasks);
   } catch (e) {
     res.status(500).send();
