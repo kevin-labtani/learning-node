7dd1e5bcb65a8bddf4e0367d0ab0d56170b97495
Kevin Labtani <kevinlabtani@gmail.com>
2019-12-23 15:56:36 +0100

    file uploading with multer

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,6 +6,17 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// multer file upload example
+const multer = require("multer");
+const upload = multer({
+  dest: "images",
+});
+
+// test with postman, with a POST req, and set a body with form-data, with key: "upload" and value "link/to/img"
+app.post("/upload", upload.single("upload"), (req, res) => {
+  res.send(); // we get a randomly generated file name with no extension uploaded to /images
+});
+
 // on dev, start db with:
 // mongod --dbpath=/home/user/Desktop/personal-projects/mead-node/task-manager/mongodb-data
 
