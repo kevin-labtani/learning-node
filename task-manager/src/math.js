d71e9232d4198cbfa06c6350c5bcba406044316f
Kevin Labtani <kevinlabtani@gmail.com>
2019-12-24 17:38:58 +0100

    initiation to Jest testing

--- task-manager/src/math.js
+++ task-manager/src/math.js
@@ -1,10 +1,16 @@
 // temp file to explore testing with jest
+const calculateTip = (total, tipPercent = 0.25) => total + total * tipPercent;
 
-const calculateTip = (total, tipPercent) => {
-  const tip = total * tipPercent;
-  return total + tip;
+const fahrenheitToCelsius = temp => {
+  return (temp - 32) / 1.8;
+};
+
+const celsiusToFahrenheit = temp => {
+  return temp * 1.8 + 32;
 };
 
 module.exports = {
   calculateTip,
+  fahrenheitToCelsius,
+  celsiusToFahrenheit,
 };
--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,10 +1,26 @@
-const { calculateTip } = require("../src/math");
+const {
+  calculateTip,
+  fahrenheitToCelsius,
+  celsiusToFahrenheit,
+} = require("../src/math");
 // run jest with npm run test
 
-// this test will pass
-test("Hello World!", () => {});
+test("Should calculate total with tip", () => {
+  const total = calculateTip(10, 0.3);
+  expect(total).toBe(13);
+});
 
-// this test will fail
-test("This should fail", () => {
-  throw new Error("Failure");
+test("Should calculate total with default tip", () => {
+  const total = calculateTip(10);
+  expect(total).toBe(12.5);
+});
+
+test("Should convert 32F to 0°c", () => {
+  const convertion = fahrenheitToCelsius(32);
+  expect(convertion).toBe(0);
+});
+
+test("Should convert O°c to 32F", () => {
+  const convertion = celsiusToFahrenheit(0);
+  expect(convertion).toBe(32);
 });
