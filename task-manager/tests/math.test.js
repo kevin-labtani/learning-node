c226d9baf063c2d2a19c64faf7f3b0146e2435ec
Kevin Labtani <kevinlabtani@gmail.com>
2019-12-24 17:52:27 +0100

    jest testing for async

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -2,7 +2,9 @@ const {
   calculateTip,
   fahrenheitToCelsius,
   celsiusToFahrenheit,
+  add,
 } = require("../src/math");
+
 // run jest with npm run test
 
 test("Should calculate total with tip", () => {
@@ -23,4 +25,25 @@ test("Should convert 32F to 0°c", () => {
 test("Should convert O°c to 32F", () => {
   const convertion = celsiusToFahrenheit(0);
   expect(convertion).toBe(32);
+});
+
+// // provide done as a param to test async function so Jest wait for done to be retruned before deciding if the test passes or fails
+// test("Async test demo", done => {
+//   setTimeout(() => {
+//     expect(1).toBe(2);
+//     done();
+//   }, 2000);
+// });
+
+test("Should add two numbers promise", done => {
+  add(2, 3).then(sum => {
+    expect(sum).toBe(5);
+    done();
+  });
+});
+
+// when the test returns a promise, Jest will wait to see if the promise is fulfilled or rejected before deciding if the test is a success or a failure
+test("Should add two numbers async/await", async () => {
+  const sum = await add(10, 22);
+  expect(sum).toBe(32);
 });
