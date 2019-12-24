9dfee423b45656cde75fe4e936f4a5a166eebf49
Kevin Labtani <kevinlabtani@gmail.com>
2019-12-24 18:27:06 +0100

    testing signup and login routes

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,5 +1,20 @@
 const request = require("supertest");
 const app = require("../src/app");
+const User = require("../src/models/user");
+
+const userOne = {
+  name: "Mike",
+  email: "mike@example.com",
+  password: "duty5689!",
+};
+
+// Jest env setup
+beforeEach(async () => {
+  // delete all users
+  await User.deleteMany();
+  // add a user for testing purpose
+  await new User(userOne).save();
+});
 
 test("Should signup a new user", async () => {
   await request(app)
@@ -10,4 +25,24 @@ test("Should signup a new user", async () => {
       password: "MyPas777!",
     })
     .expect(201);
+});
+
+test("Should login existing user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: userOne.password,
+    })
+    .expect(200);
+});
+
+test("Should not login nonexistant user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: "test@test.com",
+      password: "somepassword",
+    })
+    .expect(400);
 });
