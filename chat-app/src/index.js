f0163b3eb3b4ab01d94e29a003fafaebd73ef811
Kevin Labtani <kevinlabtani@gmail.com>
2019-12-25 15:10:42 +0100

    setup socket.io server and client side

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,8 +1,14 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
 
 // setup express
 const app = express();
+// create a web server || express typically does this behind the scenes, but socket.io expects the server to be passed to it
+const server = http.createServer(app);
+// config socketio to work with thte server, it nows supports websockets
+const io = socketio(server);
 
 const port = process.env.PORT;
 
@@ -10,5 +16,9 @@ const port = process.env.PORT;
 const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
 
+io.on("connection", () => {
+  console.log("new WebSocket connection");
+});
+
 // app is up on http://localhost:3000/
-app.listen(port, () => console.log(`Server is up on port ${port}!`));
+server.listen(port, () => console.log(`Server is up on port ${port}!`));
