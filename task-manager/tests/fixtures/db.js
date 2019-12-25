9eb6c16efe192c8eaeff7703ac9018e928a1bf80
Kevin Labtani <kevinlabtani@gmail.com>
2019-12-25 12:09:03 +0100

    test task

--- task-manager/tests/fixtures/db.js
+++ task-manager/tests/fixtures/db.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const User = require("../../src/models/user");
+const Task = require("../../src/models/task");
 
 // setup a test user
 // create an id for our test user so we can use it to generate a jwt fo rtesting purpose
@@ -17,13 +18,57 @@ const userOne = {
   ],
 };
 
+const userTwoId = new mongoose.Types.ObjectId();
+const userTwo = {
+  _id: userTwoId,
+  name: "John",
+  email: "john@example.com",
+  password: "uho679!!",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userTwoId }, process.env.JWT_SECRET),
+    },
+  ],
+};
+
+const taskOne = {
+  _id: new mongoose.Types.ObjectId(),
+  description: "First Task",
+  completed: false,
+  owner: userOne._id,
+};
+
+const taskTwo = {
+  _id: new mongoose.Types.ObjectId(),
+  description: "Second Task",
+  completed: true,
+  owner: userOne._id,
+};
+
+const taskThree = {
+  _id: new mongoose.Types.ObjectId(),
+  description: "Third Task",
+  completed: true,
+  owner: userTwo._id,
+};
+
 const setupDatabase = async () => {
   await User.deleteMany();
+  await Task.deleteMany();
   await new User(userOne).save();
+  await new User(userTwo).save();
+  await new Task(taskOne).save();
+  await new Task(taskTwo).save();
+  await new Task(taskThree).save();
 };
 
 module.exports = {
+  setupDatabase,
   userOneId,
   userOne,
-  setupDatabase,
+  userTwoId,
+  userTwo,
+  taskOne,
+  taskTwo,
+  taskThree,
 };
--- task-manager/tests/task.test.js
+++ task-manager/tests/task.test.js
@@ -1,7 +1,15 @@
 const request = require("supertest");
 const app = require("../src/app");
 const Task = require("../src/models/task");
-const { userOneId, userOne, setupDatabase } = require("./fixtures/db");
+const {
+  setupDatabase,
+  userOneId,
+  userOne,
+  userTwo,
+  taskOne,
+  taskTwo,
+  taskThree,
+} = require("./fixtures/db");
 
 // use --runInBand option for Jest to make tests run serially
 // Jest env setup
@@ -15,8 +23,30 @@ test("Should create task for user", async () => {
       description: "Test my app",
     })
     .expect(201);
-  // assert the taske xist in the db
+  // assert the task exist in the db
   const task = await Task.findById(response.body._id);
   expect(task).not.toBeNull();
   expect(task.completed).toBe(false);
+});
+
+test("Should get tasks for user", async () => {
+  const response = await request(app)
+    .get("/tasks")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .expect(200);
+
+  // we have 2 tasks for userOne
+  expect(response.body.length).toBe(2);
+});
+
+test("Should not delete task not owned by user", async () => {
+  await request(app)
+    .delete(`/task/${taskOne._id}`)
+    .set("Authorization", `Bearer ${userTwo.tokens[0].token}`)
+    .send()
+    .expect(404);
+
+  // assert the task is stil in the db
+  const task = await Task.findById(taskOne._id);
+  expect(task).not.toBeNull()
 });
