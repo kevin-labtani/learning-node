ecc92cefc9fef478ce5bf95ef88c478b39e0c27b
Kevin Labtani <kevinlabtani@gmail.com>
2019-12-25 11:35:40 +0100

    test image uploads and patch route

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -106,3 +106,41 @@ test("Should not delete account for unauthenticated user", async () => {
     .send()
     .expect(401);
 });
+
+test("Should upload avatar image", async () => {
+  await request(app)
+    .post("/users/me/avatar")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .attach("avatar", "tests/fixtures/profile-pic.jpg")
+    .expect(200);
+
+  // assert the picture is present in the db
+  const user = await User.findById(userOneId);
+  // toBe uses ===, won't work with objects
+  expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test("Should update valid user fields", async () => {
+  await request(app)
+    .patch("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      name: "Bob",
+    })
+    .expect(200);
+
+  // assert the name is changed in the db
+  const user = await User.findById(userOneId);
+  // toBe uses ===, won't work with objects
+  expect(user.name).toBe("Bob");
+});
+
+test("Should not update invalid user fields", async () => {
+  await request(app)
+    .patch("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      location: "Nivelles",
+    })
+    .expect(400);
+});
