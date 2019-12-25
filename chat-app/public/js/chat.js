e41f6013a09659189bbc5a3c0afe5766ae98b93a
Kevin Labtani <kevinlabtani@gmail.com>
2019-12-25 16:29:19 +0100

    event acknowledgements

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -3,27 +3,39 @@ const form = document.querySelector("#message-form");
 // connect to sockets.io client side
 const socket = io();
 
-// get data from server
+// get message from server
 socket.on("message", message => {
   console.log(message);
 });
 
-// send data to server
+// send message to server
 form.addEventListener("submit", e => {
   e.preventDefault();
   const message = e.target.elements.message.value;
-  socket.emit("sendMessage", message);
+  socket.emit("sendMessage", message, error => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message delivered!");
+  });
 });
 
+// send location
 document.querySelector("#send-location").addEventListener("click", () => {
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser");
   }
   // API doesn't support promises
   navigator.geolocation.getCurrentPosition(position => {
-    socket.emit("sendLocation", {
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude,
-    });
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      () => {
+        console.log("Location shared!");
+      },
+    );
   });
 });
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -2,10 +2,11 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const Filter = require("bad-words");
 
 // setup express
 const app = express();
-// create a web server || express typically does this behind the scenes, but socket.io expects the server to be passed to it
+// create a web server; express typically does this behind the scenes, but socket.io expects the server to be passed to it
 const server = http.createServer(app);
 // config socketio to work with thte server, it nows supports websockets
 const io = socketio(server);
@@ -25,17 +26,27 @@ io.on("connection", socket => {
   socket.broadcast.emit("message", "A new user has joined");
 
   // listen for client msg
-  socket.on("sendMessage", message => {
+  socket.on("sendMessage", (message, callback) => {
+    // init bad words filter
+    const filter = new Filter();
+    // don't send the message if it contains profanity
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed");
+    }
     // send msg to everyone
     io.emit("message", message);
+    // acknowlegement message was send
+    callback();
   });
   // listen for client location
-  socket.on("sendLocation", coords => {
+  socket.on("sendLocation", (coords, callback) => {
     // send msg to everyone
     io.emit(
       "message",
       `https://google.com/maps?q=${coords.latitude},${coords.longitude}`,
     );
+      // acknoledgement location was shared
+    callback()
   });
 
   // send msg on disconnect
